f1da745c6b87981fc09f986fffbb620eb82187d7
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-09-09 13:26:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Satan-tg/Not-repo.git /Not-repo
+  git clone https://github.com/afluaflu123/Tombot.git /Tombot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Not-repo
+  git clone $UPSTREAM_REPO /Tombot
 fi
-cd /Not-repo
+cd /Tombot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
